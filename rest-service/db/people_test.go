6d1ee55fb5c7cf9b791e858f7d883c91bbf862a0
Nicolas Andreoli <nicolasandreoli9@gmail.com>
2022-05-27 17:54:46 -0300

    finish db layer unit test

--- rest-service/db/people_test.go
+++ rest-service/db/people_test.go
@@ -1,6 +1,9 @@
 package db_test
 
 import (
+	"fmt"
+	uuid "github.com/satori/go.uuid"
+	"server/db/models"
 	"testing"
 
 	"server/db"
@@ -27,5 +30,32 @@ func (p *PeopleTestSuite) TestGetAllPeopleSuccess() {
 	expected := mocks.People
 	people := p.peopleDB.AllPeople()
 
+	assert.Equal(p.T(), expected, people)
+}
+
+func (p *PeopleTestSuite) TestFindPersonByIDSuccess() {
+	expected := mocks.People[0]
+	people, err := p.peopleDB.FindPersonByID(uuid.FromStringOrNil("81eb745b-3aae-400b-959f-748fcafafd81"))
+	assert.Nil(p.T(), err)
+	assert.Equal(p.T(), expected, people)
+}
+
+func (p *PeopleTestSuite) TestFindPersonByIDNotFound() {
+	uuid := uuid.NewV1()
+	expected := fmt.Sprintf("user ID %s not found", uuid.String())
+	people, err := p.peopleDB.FindPersonByID(uuid)
+	assert.Nil(p.T(), people)
+	assert.Equal(p.T(), expected, err.Error())
+}
+
+func (p *PeopleTestSuite) TestFindPeopleByNamesSuccess() {
+	expected := []*models.Person{mocks.People[0], mocks.People[3]}
+	people := p.peopleDB.FindPeopleByNames("John", "Doe")
+	assert.Equal(p.T(), expected, people)
+}
+
+func (p *PeopleTestSuite) TestFindPeopleByPhoneNumberSuccess() {
+	expected := []*models.Person{mocks.People[2], mocks.People[4]}
+	people := p.peopleDB.FindPeopleByPhoneNumber("+44 7700 900077")
 	assert.Equal(p.T(), expected, people)
 }
